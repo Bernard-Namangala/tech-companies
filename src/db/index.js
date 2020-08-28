40be19745c1bdd71422a446c9931395b7d784899
Bernard-Namangala <bernardnamangala@gmail.com>
2020-08-28 12:44:47 +0200

    configuring coverrals

--- src/db/index.js
+++ src/db/index.js
@@ -4,28 +4,28 @@ import dotenv from "dotenv";
 dotenv.config();
 
 let pool = new pg.Pool({
-	connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 if (process.env.NODE_ENV === "development") {
-	console.log("happening");
-	pool = new pg.Pool({
-		user: "postgres",
-		host: "localhost",
-		database: "companies",
-		password: process.env.DATABASE_PASSWORD,
-		port: "5432",
-	});
+  console.log("happening");
+  pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "companies",
+    password: process.env.DATABASE_PASSWORD,
+    port: "5432",
+  });
 } else if (process.env.NODE_ENV === "testing") {
-	pool = new pg.Pool({
-		user: "postgres",
-		host: "localhost",
-		database: "testing_companies",
-		password: "",
-		port: "5432",
-	});
+  pool = new pg.Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "testing_companies",
+    password: process.env.DATABASE_PASSWORD,
+    port: "5432",
+  });
 }
 
 export default {
-	query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params),
 };
